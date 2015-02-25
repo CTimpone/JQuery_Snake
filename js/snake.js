d76b29a0101767feafc6cafe2be4ee494209d34d
Jacob Shorty <jakeshorty@gmail.com>
2015-02-25 16:08:07 -0500

    Simple turning logic for snake

--- js/snake.js
+++ js/snake.js
@@ -3,26 +3,36 @@
     window.SnakeGame = {};
   }
 
+  console.log("SNAKE LOADED!");
+
   var Snake = window.SnakeGame.Snake = function () {
     this.dir = "N";
-    this.segments = [Math.floor(SnakeGame.Board.DIM_X / 2), Math.floor(SnakeGame.Board.DIM_Y / 2)];
+    var head = [Math.floor(SnakeGame.Board.DIM_X / 2), Math.floor(SnakeGame.Board.DIM_Y / 2)]
+    this.segments = [head, [head[0], head[1] + 1], [head[0], head[1] + 2],
+                    [head[0], head[1] + 3], [head[0], head[1] + 4]];
     this.pivots = [];
   };
 
   Snake.prototype.move = function () {
+    var head = this.segments.slice(0, 1)[0]
     this.segments.pop();
-    var head = this.segments.slice(0, 1)
     switch (this.dir) {
       case "N":
+        console.log(head)
         this.segments.unshift([head[0], head[1] - 1]);
+        break;
       case "S":
         this.segments.unshift([head[0], head[1] + 1]);
+        break;
       case "E":
         this.segments.unshift([head[0] + 1, head[1]]);
+        break;
       case "W":
         this.segments.unshift([head[0] - 1, head[1]]);
+        break;
+      default:
+        break
     }
-    })
   };
 
   Snake.prototype.turn = function (direction) {
@@ -31,22 +41,35 @@
 
   var Board = window.SnakeGame.Board = function () {
     this.snake = new Snake();
-  }
-
-  Board.DIM_X = 100;
-  Board.DIM_Y = 100;
-
-  var Coord = window.SnakeGame.Coord = function () {};
-
-  // Coord.plus = function (pos1, pos2) {
-  //   return [pos1[0] + pos2[0], pos1[1] + pos2[1]];
-  // };
-
-  Coord.equals = function () {
-
+    this.setupGrid();
   };
 
-  Coord.isOpposite = function () {
+  Board.DIM_X = 10;
+  Board.DIM_Y = 10;
 
+  Board.prototype.setupGrid = function() {
+    var arr = [];
+    for (var i = 0; i < Board.DIM_Y; i++) {
+      arr.push([]);
+      for (var j = 0; j < Board.DIM_X; j++) {
+        arr[i][j] = ".";
+      }
+    }
+    this.grid = arr;
   };
+
+  Board.prototype.render = function () {
+    this.setupGrid();
+    for (var i = 0; i < this.snake.segments.length; i++) {
+       this.grid[this.snake.segments[i][1]][this.snake.segments[i][0]] = "S";
+    }
+
+    return JSON.stringify(this.grid);
+  };
+
+  Board.prototype.step = function () {
+    this.snake.move();
+    this.render();
+  };
+
 })();
