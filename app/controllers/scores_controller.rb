648762347ef554e30a50ffbb2307dcb56a6e1624
Chris Timpone <ChrisTimpone@gmail.com>
2015-03-25 16:40:16 -0400

    Improve table.

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,15 +1,18 @@
 class ScoresController < ApplicationController
   def create
     @score = Score.new(score_params)
+    if @score.name.length > 15
+      @score.name = @score.name[0..14]
+    end
     if @score.save
-      render json: Score.top_twelve
+      render json: Score.top_eleven
     else
       render json: {errors: @score.errors.full_messages}
     end
   end
 
   def index
-    render json: Score.top_twelve
+    render json: Score.top_eleven
   end
 
   private
--- app/models/score.rb
+++ app/models/score.rb
@@ -1,7 +1,7 @@
 class Score < ActiveRecord::Base
   validates :name, :score, :speed, presence: true
 
-  def self.top_twelve
-    Score.where("score > 0").order(score: :desc).limit(12)
+  def self.top_eleven
+    Score.where("score > 0").order(score: :desc).limit(11)
   end
 end
